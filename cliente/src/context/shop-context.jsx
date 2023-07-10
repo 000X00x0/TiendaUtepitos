ec113b84632d12c2be4b103873d08bbcf2bdce76
Di3go0-0 <d.rincon@utp.edu.co>
2023-07-10 03:02:44 -0500

    commit

--- cliente/src/context/shop-context.jsx
+++ cliente/src/context/shop-context.jsx
@@ -13,66 +13,74 @@ const getDefaultCart = () => {  // Función para obtener un carrito por defecto 
     return cart;  // Retornamos el carrito por defecto con las cantidades de productos establecidas en cero
 };
 
-
 export const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState(getDefaultCart()); //aqui se almacenaran los productos ingresados dentro del carrito
-    const [payAumount,setPayAumount] = useState(0);//aqui se guarda el total de la compra
+    const [cartItems, setCartItems] = useState(getDefaultCart());  // Estado para almacenar los productos ingresados en el carrito, inicializado con el carrito por defecto
+    const [payAumount, setPayAumount] = useState(0);  // Estado para almacenar el monto total de la compra, inicializado en cero
 
-    const[products, setProducts] = useState([])//aqui va a obtener todos los productos que se encuentran dentro de la base de datos
+    const [products, setProducts] = useState([]);  // Estado para almacenar todos los productos obtenidos de la base de datos, inicializado como un arreglo vacío
     useEffect(() => {
-        getProducts()
+        getProducts();  // Obtenemos los productos al cargar el componente
     }, []);
 
-    const [logged, setLogged] = useState(0);//este hook es para saber si hay un usuario logeado en la pagina
-    const loggedChanger = (value) => setLogged(value);//con esto se le cambia el valor al hook logged 
+    const [logged, setLogged] = useState(0);  // Estado para verificar si hay un usuario logueado en la página, inicializado en cero
+    const loggedChanger = (value) => setLogged(value);  // Función para cambiar el valor del estado 'logged'
 
-    const [admin, setAdmin] = useState(false);//se utiliza para saber si el usuario es un admin, por defecto el hook se encuentra en falso
-    const AdminChanger = (value) => setAdmin(value);
-    
-    const getProducts = async () => {//aqui se hacen las peticiones a la base de datos para obtener todos los productos
-        const res = await axios.get(URI)
-        setProducts(res.data);
+    const [admin, setAdmin] = useState(false);  // Estado para verificar si el usuario es un administrador, inicializado en falso
+    const AdminChanger = (value) => setAdmin(value);  // Función para cambiar el valor del estado 'admin'
 
+    const getProducts = async () => {  // Función asincrónica para obtener todos los productos de la base de datos
+        const res = await axios.get(URI);  // Realizamos una petición GET a la URL especificada para obtener los productos
+        setProducts(res.data);  // Actualizamos el estado 'products' con los datos obtenidos de la base de datos
     }
 
-
-    const getTotalCartAmount = () => {//esta funcion permite dabe
-        let totalAmount = 0;//se crea una variable con el total que comenzara en 0
-        for (const item in cartItems) {//se crea una bucle que recorre cada item del arreglo carItems 
-            if (cartItems[item] > 0) { //se pregunta si el valor del en la posicion item de ese arreglo es mayor de 0
-                let itemInfo = products.find((product) => product.id === Number(item)); //dentro de una variable se almacena el producto para poder usar el precio mas adelante
-                totalAmount += cartItems[item] * itemInfo.precio; //dentro de totalAmount se almacena la multiplicacion del precio del producto multiplicado por la cantidad de productos que se encuentra del arreglo
+    const getTotalCartAmount = () => {  // Función para obtener el monto total de la compra en el carrito
+        let totalAmount = 0;  // Variable para almacenar el monto total, inicializada en cero
+        for (const item in cartItems) {  // Iteramos sobre los items del carrito
+            if (cartItems[item] > 0) {  // Verificamos si la cantidad del item es mayor a cero
+                let itemInfo = products.find((product) => product.id === Number(item));  // Obtenemos la información del producto correspondiente al item
+                totalAmount += cartItems[item] * itemInfo.precio;  // Calculamos el subtotal multiplicando la cantidad por el precio del producto
             }
         }
- 
-        return totalAmount;//retorna el valor total de la compra 
+        return totalAmount;  // Retornamos el monto total de la compra
     };
 
-    const addToCart = async (itemId) => { //funcion para poder agregar al carrito enviando como parametro el id del producto y poder reservarlo en el servidor
-        await axios.get('http://localhost:3001/products/book/'+ itemId + '?f=book')//se genera una peticion get para poder traer el el producto el cual se va reservar el producto
+    const addToCart = async (itemId) => {  // Función para agregar un producto al carrito, recibiendo como parámetro el ID del producto
+        await axios.get('http://localhost:3001/products/book/'+ itemId + '?f=book')  // Realizamos una petición GET para reservar el producto en el servidor
         .then(({ data }) => {
-            data==='Booked' ? setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1 })) : void(0);//si el dato extraido es Booked le sumamos 1 a la posicion que represente al producto dentro del arreglo para poder saber la cantidad de cada producto
-            data==='Stockout' ? alert('Empty product') : void(0); //en caso de que el estado retornado sea Stockout se crea una alerta que dice que el producto esta vacio y no hace nada
+            data === 'Booked' ? setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1 })) : void(0);  // Si el estado retornado es 'Booked', incrementamos la cantidad del producto en el carrito
+            data === 'Stockout' ? alert('Empty product') : void(0);  // Si el estado retornado es 'Stockout', mostramos una alerta indicando que el producto está vacío
         })
         .catch(error => {
-            console.log(error.message);//si hay un error lo muestra por consola
+            console.log(error.message);  // Si ocurre un error, lo mostramos en la consola
         }) 
     };
 
-    const removeFromCart = async (itemId) => { //funcion para remover del carrito a partir del id
-        await axios.get('http://localhost:3001/products/book/'+ itemId + '?f=unbook')//en esta ruta se hace la peticion
+    const removeFromCart = async (itemId) => {  // Función para remover un producto del carrito, recibiendo como parámetro el ID del producto
+        await axios.get('http://localhost:3001/products/book/'+ itemId + '?f=unbook')  // Realizamos una petición GET para cancelar la reserva del producto en el servidor
         .then(({ data }) => {
-            data==='Unbooked' ? setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1 })) : void(0);//si tiene el estado unbooked se le resta 1 en el arreglo de la cantidad
+            data === 'Unbooked' ? setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1 })) : void(0);  // Si el estado retornado es 'Unbooked', decrementamos la cantidad del producto en el carrito
         })
         .catch(error => {
-            console.log(error.message);
+            console.log(error.message);  // Si ocurre un error, lo mostramos en la consola
         }) 
     };
 
-    const contextValue = { cartItems, addToCart, removeFromCart, getTotalCartAmount, loggedChanger, logged, AdminChanger, admin, payAumount,setPayAumount};//metemos todas las funciones y hooks dentro del contexto
+    const contextValue = {  // Objeto que contiene todas las funciones y estados que se compartirán en el contexto
+        cartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount,
+        loggedChanger,
+        logged,
+        AdminChanger,
+        admin,
+        payAumount,
+        setPayAumount
+    };
+
     return (
         <ShopContext.Provider value={contextValue}>
-            {props.children}
+            {props.children}  // Renderizamos los componentes hijos que se pasen a través de las propiedades
         </ShopContext.Provider>
     );
 };
